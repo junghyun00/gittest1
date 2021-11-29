e3fb2ed36a15b9b44b5ded6e85db8c7e1b62b3d2
junghyun00 <ojh8958@naver.com>
2021-11-29 10:13:29 +0900

    첫번째 커밋

---
+++ src/test/java/dev/mvc/gittest1/Gittest1ApplicationTests.java
@@ -0,0 +1,14 @@
+package dev.mvc.gittest1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootTest
+class Gittest1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
